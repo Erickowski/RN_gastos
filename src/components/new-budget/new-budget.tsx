30179c28f0f075c73ac9368441c45321882e3f89
Erick Mata <erickdanielperezmata@outlook.es>
2023-07-02 19:12:02 -0600

    Validate budget value

--- src/components/new-budget/new-budget.tsx
+++ src/components/new-budget/new-budget.tsx
@@ -1,8 +1,15 @@
 import { Text, Pressable, View, TextInput } from "react-native";
+import { useState } from "react";
 
 import styles from "./styles";
 
-export const NewBudget = () => {
+type Props = {
+  onSetBudget: (budget: string) => void;
+};
+
+export const NewBudget = ({ onSetBudget }: Props) => {
+  const [budget, setBudget] = useState<string>("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Nuevo presupuesto</Text>
@@ -11,9 +18,11 @@ export const NewBudget = () => {
         keyboardType="numeric"
         placeholder="Agrega tu presupuesto: Ej. 300"
         style={styles.input}
+        value={budget}
+        onChangeText={setBudget}
       />
 
-      <Pressable style={styles.button}>
+      <Pressable style={styles.button} onPress={() => onSetBudget(budget)}>
         <Text style={styles.textButton}>Agregar presupuesto</Text>
       </Pressable>
     </View>
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,15 +1,23 @@
-import { View } from "react-native";
+import { View, Alert } from "react-native";
 
 import { Header, NewBudget } from "@src/components";
 
 import styles from "./styles";
 
 export function Home() {
+  const handleSetBudget = (budget: string) => {
+    if (Number(budget) > 0) {
+      console.log(budget);
+    } else {
+      Alert.alert("Error", "El presupuesto no puede ser 0 o menor");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Header />
-        <NewBudget />
+        <NewBudget onSetBudget={handleSetBudget} />
       </View>
     </View>
   );
