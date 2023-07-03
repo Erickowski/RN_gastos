34a481c7d2d33ee909680cfaacfef66fa0e34a3b
Erick Mata <erickdanielperezmata@outlook.es>
2023-07-02 23:35:52 -0600

    Create fields for add bill modal

--- src/components/add-bill-modal/add-bill-modal.tsx
+++ src/components/add-bill-modal/add-bill-modal.tsx
@@ -1,4 +1,14 @@
-import { Modal, Text, SafeAreaView } from "react-native";
+import {
+  Pressable,
+  Modal,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+import { PICKER_ITEMS } from "./constants";
 
 type Props = {
   visible: boolean;
@@ -8,7 +18,41 @@ export const AddBillModal = ({ visible }: Props) => {
   return (
     <Modal visible={visible} animationType="slide">
       <SafeAreaView>
-        <Text>Modal</Text>
+        <View>
+          <Pressable>
+            <Text>Cancelar</Text>
+          </Pressable>
+        </View>
+
+        <View>
+          <Text>Nuevo gasto</Text>
+
+          <View>
+            <Text>Nombre Gasto</Text>
+            <TextInput placeholder="Nombre del gasto, ej: comida" />
+          </View>
+
+          <View>
+            <Text>Cantidad Gasto</Text>
+            <TextInput
+              keyboardType="numeric"
+              placeholder="Cantidad del gasto, ej: 300"
+            />
+          </View>
+
+          <View>
+            <Text>Categoria gasto</Text>
+            <Picker>
+              {PICKER_ITEMS.map((item) => (
+                <Picker.Item {...item} key={item.value} />
+              ))}
+            </Picker>
+          </View>
+
+          <Pressable>
+            <Text>Agregar gasto</Text>
+          </Pressable>
+        </View>
       </SafeAreaView>
     </Modal>
   );
---
+++ src/components/add-bill-modal/constants.ts
@@ -0,0 +1,31 @@
+export const PICKER_ITEMS = [
+  { label: "-- Seleccione --", value: "" },
+  {
+    label: "Ahorro",
+    value: "ahorro",
+  },
+  {
+    label: "Comida",
+    value: "comida",
+  },
+  {
+    label: "Casa",
+    value: "casa",
+  },
+  {
+    label: "Gastos Varios",
+    value: "gastos",
+  },
+  {
+    label: "Ocio",
+    value: "ocio",
+  },
+  {
+    label: "Salud",
+    value: "salud",
+  },
+  {
+    label: "Suscripciones",
+    value: "suscripciones",
+  },
+];
