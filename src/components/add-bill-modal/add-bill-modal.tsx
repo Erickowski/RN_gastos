cf4faa122cc58a6c22323cee4b153948543da6f7
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2023-12-24 21:17:42 -0600

    add styles to form add bill modal

--- src/components/add-bill-modal/add-bill-modal.tsx
+++ src/components/add-bill-modal/add-bill-modal.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
+import styles from "./styles";
 import { PICKER_ITEMS } from "./constants";
 
 type Props = {
@@ -17,31 +18,35 @@ type Props = {
 export const AddBillModal = ({ visible }: Props) => {
   return (
     <Modal visible={visible} animationType="slide">
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <View>
           <Pressable>
             <Text>Cancelar</Text>
           </Pressable>
         </View>
 
-        <View>
-          <Text>Nuevo gasto</Text>
+        <View style={styles.form}>
+          <Text style={styles.title}>Nuevo gasto</Text>
 
-          <View>
-            <Text>Nombre Gasto</Text>
-            <TextInput placeholder="Nombre del gasto, ej: comida" />
-          </View>
-
-          <View>
-            <Text>Cantidad Gasto</Text>
+          <View style={styles.label}>
+            <Text style={styles.labelText}>Nombre Gasto</Text>
             <TextInput
-              keyboardType="numeric"
-              placeholder="Cantidad del gasto, ej: 300"
+              placeholder="Nombre del gasto, ej: comida"
+              style={styles.input}
             />
           </View>
 
-          <View>
-            <Text>Categoria gasto</Text>
+          <View style={styles.label}>
+            <Text style={styles.labelText}>Cantidad Gasto</Text>
+            <TextInput
+              keyboardType="numeric"
+              placeholder="Cantidad del gasto, ej: 300"
+              style={styles.input}
+            />
+          </View>
+
+          <View style={styles.label}>
+            <Text style={styles.labelText}>Categoria gasto</Text>
             <Picker>
               {PICKER_ITEMS.map((item) => (
                 <Picker.Item {...item} key={item.value} />
@@ -49,8 +54,8 @@ export const AddBillModal = ({ visible }: Props) => {
             </Picker>
           </View>
 
-          <Pressable>
-            <Text>Agregar gasto</Text>
+          <Pressable style={styles.submitButton}>
+            <Text style={styles.submitText}>Agregar gasto</Text>
           </Pressable>
         </View>
       </SafeAreaView>
--- src/components/add-bill-modal/styles.ts
+++ src/components/add-bill-modal/styles.ts
@@ -1,5 +1,47 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+import { GLOBAL_STYLES } from "@src/styles";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1E40AF",
+    flex: 1,
+  },
+  form: {
+    ...GLOBAL_STYLES.container,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    marginBottom: 30,
+    color: "#64748B",
+  },
+  label: {
+    marginVertical: 10,
+  },
+  labelText: {
+    color: "#64748B",
+    textTransform: "uppercase",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#f5f5f5",
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  submitButton: {
+    backgroundColor: "#3b82f6",
+    padding: 10,
+    marginTop: 20,
+  },
+  submitText: {
+    textAlign: "center",
+    color: "#FFF",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+});
 
 export default styles;
